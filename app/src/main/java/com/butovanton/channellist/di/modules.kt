80d340d457bc41bfb538081c749cd7da3d595c49
Anton Butov <butov6101@gmail.com>
2024-01-13 15:23:08 +0300

    second commit

--- app/src/main/java/com/butovanton/channellist/di/modules.kt
+++ app/src/main/java/com/butovanton/channellist/di/modules.kt
@@ -6,15 +6,28 @@ import com.butovanton.channellist.data.IService
 import com.butovanton.channellist.data.Repository
 import com.butovanton.channellist.domain.IRepository
 import com.butovanton.channellist.presentation.TabsViewModel
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 private const val baseUrl = "https://limeapi.online/api/"
+private const val apiKey = "fh3487klskhjk2fh782kjhsdi72knjwfk7i2efdjbm"
 
 val repositoryModule = module {
+    val client = OkHttpClient().newBuilder().addInterceptor(object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val requestBuilder = chain.request().newBuilder()
+                requestBuilder.header("X-Key", apiKey)
+                return chain.proceed(requestBuilder.build())
+            }
+        }).build()
     val retrofit = Retrofit.Builder()
+        .client(client)
         .baseUrl(baseUrl)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
