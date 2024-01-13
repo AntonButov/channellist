4e4efeb9e58e4cbab9aad20a2f626d547289a72a
Anton Butov <butov6101@gmail.com>
2024-01-13 16:20:53 +0300

    second commit

--- app/src/main/java/com/butovanton/channellist/data/FavoriteRepository.kt
+++ app/src/main/java/com/butovanton/channellist/data/FavoriteRepository.kt
@@ -2,10 +2,11 @@ package com.butovanton.channellist.data
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 
 class FavoriteRepository: IFavoriteRepository {
     override fun getFavorites(): Flow<List<String>> {
-        return emptyFlow()
+        return flowOf(emptyList())
     }
 
     override fun add(name: String) {
