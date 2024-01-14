e702961534018642dbd760eb618f597508e1177f
Anton Butov <butov6101@gmail.com>
2024-01-14 11:08:13 +0300

    second commit

--- app/src/main/java/com/butovanton/channellist/PlayerActivity.kt
+++ app/src/main/java/com/butovanton/channellist/PlayerActivity.kt
@@ -6,15 +6,17 @@ import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.common.MediaItem.fromUri
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
@@ -23,23 +25,19 @@ class PlayerActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyPlayer()
-                }
+            Player()
         }
     }
 }
 
 @Composable
-fun MyPlayer() {
-  //  val hlsUri = Uri.parse("https:\\/\\/mhd.iptv2022.com\\/p\\/uIADBnajR1ufdeqQR98g6g,1703196009\\/streaming\\/muztv\\/324\\/1\\/index.m3u8")
-  //  val hlsUri = Uri.parse("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
+fun Player() {
+    //  val hlsUri = Uri.parse("https:\\/\\/mhd.iptv2022.com\\/p\\/uIADBnajR1ufdeqQR98g6g,1703196009\\/streaming\\/muztv\\/324\\/1\\/index.m3u8")
+    //  val hlsUri = Uri.parse("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
     val hlsUri = Uri.parse("https://albportal.net/albkanalemusic.m3u8")
     val context = LocalContext.current
+    val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
+
     val player: ExoPlayer = remember {
         ExoPlayer.Builder(context)
             .build()
@@ -58,11 +56,27 @@ fun MyPlayer() {
             }
         }
     )
-}
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MaterialTheme {
-        MyPlayer()
+
+    DisposableEffect(player) {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_RESUME -> {
+                    player.play()
+                }
+                Lifecycle.Event.ON_PAUSE -> {
+                    player.stop()
+                }
+                else -> {}
+            }
+        }
+
+        val lifecycle = lifecycleOwner.value.lifecycle
+        lifecycle.addObserver(observer)
+        onDispose {
+            player.release()
+            lifecycle.removeObserver(observer)
+        }
     }
+
+
 }
