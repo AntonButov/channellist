95d1560e8c5956bae29ffd3040828b8449b1097e
Anton Butov <butov6101@gmail.com>
2024-01-14 12:08:28 +0300

    second commit

--- app/src/main/java/com/butovanton/channellist/presentation/components/ChannelItem.kt
+++ app/src/main/java/com/butovanton/channellist/presentation/components/ChannelItem.kt
@@ -1,36 +1,22 @@
 package com.butovanton.channellist.presentation.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import com.butovanton.chanellist.R
 import com.butovanton.channellist.presentation.theme.ChannelListTheme
 
 @Composable
@@ -38,7 +24,7 @@ fun ChannelItem(
     modifier: Modifier = Modifier,
     name: String,
     icon: String?,
-    isFavorite: Boolean,
+    isFavorite: Boolean?,
     onFavoriteClick: () -> Unit,
     onClick: () -> Unit
 ) {
@@ -59,9 +45,25 @@ fun ChannelItem(
             Text(text = name)
         },
         trailingContent = {
-            FavoriteIcon(isChecked = isFavorite, onClick = onFavoriteClick)
+            isFavorite?.let {
+                FavoriteIcon(isChecked = isFavorite, onClick = onFavoriteClick)
+            }
         }
     )
+}
+
+@Preview()
+@Composable
+fun ChannelItemPreviewChecked() {
+    ChannelListTheme {
+        ChannelItem(
+            name = "name",
+            isFavorite = true,
+            onFavoriteClick = {},
+            icon = null,
+            onClick = {}
+        )
+    }
 }
 
 @Preview()
@@ -70,7 +72,7 @@ fun ChannelItemPreview() {
     ChannelListTheme {
         ChannelItem(
             name = "name",
-            isFavorite = true,
+            isFavorite = null,
             onFavoriteClick = {},
             icon = null,
             onClick = {}
