f6812b2229f24f952177a45fc949d55762d83581
Anton Butov <butov6101@gmail.com>
2024-01-14 17:30:30 +0300

    second commit

--- app/src/main/java/com/butovanton/channellist/presentation/components/Search.kt
+++ app/src/main/java/com/butovanton/channellist/presentation/components/Search.kt
@@ -60,7 +60,9 @@ fun Search(
         ),
         maxLines = 1,
         singleLine = true,
-        placeholder = { Text(text = placeHolderText, style = MaterialTheme.typography.bodySmall) },
+        placeholder = {
+            Text(text = placeHolderText, style = MaterialTheme.typography.bodySmall)
+        },
         shape = MaterialTheme.shapes.large
     )
 }
