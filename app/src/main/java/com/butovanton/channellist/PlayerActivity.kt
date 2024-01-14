ed838f403ffdfb639617d1470d79298447b4acd9
Anton Butov <butov6101@gmail.com>
2024-01-14 15:44:42 +0300

    second commit

--- app/src/main/java/com/butovanton/channellist/PlayerActivity.kt
+++ app/src/main/java/com/butovanton/channellist/PlayerActivity.kt
@@ -11,7 +11,10 @@ import androidx.activity.compose.setContent
 import androidx.annotation.OptIn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,10 +29,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -38,6 +43,7 @@ import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
+import coil.compose.AsyncImage
 import com.butovanton.channellist.presentation.theme.ChannelListTheme
 
 private const val CHANNEL_URL = "chanel_url"
@@ -61,7 +67,20 @@ class PlayerActivity : ComponentActivity() {
                                 titleContentColor = MaterialTheme.colorScheme.onSurface,
                             ),
                             title = {
-                                Text("Small Top App Bar")
+                                Row(
+                                    modifier = Modifier.padding(all = 12.dp),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    AsyncImage(
+                                        modifier = Modifier.size(44.dp),
+                                        model = icon,
+                                        contentDescription = null
+                                    )
+                                    Spacer(modifier = Modifier.size(24.dp))
+                                    Text(
+                                        text = text
+                                    )
+                                }
                             },
                             navigationIcon = {
                                 IconButton(onClick = { finish() }) {
