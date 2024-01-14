4fb314ba9f5dae50f84fd6a20d110b8f33b644b8
Anton Butov <butov6101@gmail.com>
2024-01-14 12:26:41 +0300

    second commit

--- app/src/main/java/com/butovanton/channellist/PlayerActivity.kt
+++ app/src/main/java/com/butovanton/channellist/PlayerActivity.kt
@@ -7,10 +7,25 @@ import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.OptIn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
@@ -24,13 +39,37 @@ import androidx.media3.ui.PlayerView
 import com.butovanton.channellist.presentation.theme.ChannelListTheme
 
 class PlayerActivity : ComponentActivity() {
+    @kotlin.OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val url = intent.getStringExtra(CHANNEL_URL) ?: throw Exception("No url")
         setContent {
-                ChannelListTheme {
-                    Player(url)
+            ChannelListTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Color.Transparent,
+                                titleContentColor = MaterialTheme.colorScheme.onSurface,
+                            ),
+                            title = {
+                                Text("Small Top App Bar")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { finish() }) {
+                                    Icon(
+                                        imageVector = Icons.Default.ArrowBack,
+                                        contentDescription = null
+                                    )
+                                }
+                            }
+
+                        )
+                    },
+                ) { innerPadding ->
+                    Player(url = url)
                 }
+            }
         }
     }
 
@@ -40,7 +79,7 @@ class PlayerActivity : ComponentActivity() {
 }
 
 @OptIn(UnstableApi::class) @Composable
-fun Player(url: String) {
+fun Player(modifier: Modifier = Modifier, url: String) {
     val hardCodedUrl = Uri.parse("https://albportal.net/albkanalemusic.m3u8") // todo remove when fix backend
     val context = LocalContext.current
     val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
@@ -56,6 +95,7 @@ fun Player(url: String) {
     }
 
     AndroidView(
+        modifier = modifier,
         factory = {
             PlayerView(context).apply {
                 this.player = player
